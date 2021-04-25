4d8ab5767319762f93b706d37f4a716428a0cf8c
J Roupie <j.roupie.93@gmail.com>
2021-04-25 17:28:53 +0100

    Using the Vue CLI

---
+++ section_7/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+  <h2>My Friends</h2>
+  <ul>
+    <friend-contact></friend-contact>
+  </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id:'a',
+          name: 'b',
+          phone: '1234',
+          email: 'c'
+        },
+        {
+          id:'a',
+          name: 'b',
+          phone: '1234',
+          email: 'c'
+        }
+      ]		
+    }
+  }
+};
+</script>
